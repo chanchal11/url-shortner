288d54ad3d42ff3ac6804786d1527ce348dc51e8
chanchal-exotel <chanchal.kumar@exotel.in>
2022-04-23 11:02:03 +0530

    association

--- src/data/association.js
+++ src/data/association.js
@@ -1,5 +1,7 @@
 function applyAssociation(sequelize) {
-
+    const { shortcode, statistic } = sequelize.models;
+     
+    statistic.belongsTo(shortcode);
 }
 
 module.exports = { applyAssociation };
